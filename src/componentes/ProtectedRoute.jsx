131c1427adb739483779ca3d103c57cd79668b7a
Artli22 <ludinlima2514@gmail.com>
2026-05-29 08:36:58 -0600

    Ajuste rapdio al router

--- src/componentes/ProtectedRoute.jsx
+++ src/componentes/ProtectedRoute.jsx
@@ -2,15 +2,13 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { loginServicio } from '../servicios/loginServicio';
 
 export const ProtectedRoute = ({ requiredRoles }) => {
-  // Obtener rol del usuario actual
   const userRol = loginServicio.obtenerRol();
 
-  // Si no hay rol, usuario no autenticado
   if (!userRol) {
     return <Navigate to="/" replace />;
   }
 
-  // Verificar si el rol actual está en los roles permitidos
+
   const isAuthorized = requiredRoles.includes(userRol);
 
   if (!isAuthorized) {
@@ -27,11 +25,9 @@ export const ProtectedRoute = ({ requiredRoles }) => {
       }}>
         <h1>Acceso Denegado</h1>
         <p>Tu rol ({userRol}) no tiene permisos para acceder a esta sección.</p>
-        <p>Contacta al administrador si crees que es un error.</p>
       </div>
     );
   }
 
-  // Si está autorizado, renderizar las rutas hijas
   return <Outlet />;
 };
